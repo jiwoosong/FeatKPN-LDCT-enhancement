b174a26fee816168e8c4c3c68c8c5cd0dd9cef46
jiwoosong <adrenaline0097@gmail.com>
2025-09-16 21:02:32 +0900

    initial update

--- README.md
+++ README.md
@@ -2,9 +2,90 @@
 
 ---
 
-This repository includes implementation of : Feature-space Kernel Prediction Network 
-for Denoising of Low-dose Brain CT.
+This repository provides the implementation of: 
+Feature-space Kernel Prediction Network with Anatomical Perception for Denoising of Low-dose Brain CT
 
-```python
+### Architecture
 
+<div align="center">
+    <img src="resources/arch.png" width="1000" />
+</div>
+
+**Feat-KPN** extracts multi-scale features from LDCT, 
+predicts spatially adaptive kernels $K^{0,1,2}$, applies them to local features, 
+and reconstructs the denoised CT image via progressive deconvolution.
+Since CT noise typically shows a non-uniform, radially distributed pattern, 
+spatially adaptive denoising provides a clear advantage over conventional CNNs with fixed kernels.
+
+### Anatomical Perception Loss
+
+<div align="center">
+    <img src="resources/loss_guide.png" width="550" />
+</div>
+
+We integrates the following components 1) L1 loss to alleviate over-smoothing effects, 2) latent-space alignment with a pre-trained
+brain NDCT model to enhance perceptual fidelity, and 3) segmentation-guided learning to preserve anatomical structure.
+
+## Installation
+
+---
+```bash
+git clone https://github.com/jiwoosong/FeatKPN-LDCT-enhancement.git
+cd FeatKPN-LDCT-enhancement
+pip install -r requirements.txt
 ```
+* Dependency
+  * Python >= 3.9
+
+## Preparation
+
+---
+### Dataset Preparation
+We follow the protocol described in the paper using the Low-Dose CT and Projection Dataset:
+1. Download the dataset from [AAPM Low Dose CT Grand Challenge 2016](https://www.aapm.org/GrandChallenge/LowDoseCT/).
+2. Convert Dicom series into NIFTI format.
+   ```bash
+   ./Data/
+    ├── 2_NIFTI_Data
+        ├── N005
+            ├── full.nii.gz
+            └── low.nii.gz
+        ├── ...
+        └── N300
+   ```
+3. Adjust `--data_path` and `--subject_path` arguments if using a different directory.
+
+### Anatomical Segmentation Model Preparation
+The LAP loss uses a pre-trained head CT segmentation model : [Fully Automated Segmentation of Head CT Neuroanatomy Using Deep Learning](https://doi.org/10.1148/ryai.2020190183)
+This project is implemented in [github page](https://github.com/jasonccai/HeadCTSegmentation) with Tensorflow.
+
+We reimplement this project into pytorch. Download [converted model weight](https://1drv.ms/u/c/7ecb33c3ddef116b/ETvrNGKp4utIhgp--vaTf0EBqNVP2QVE7AYwAbv0QP8ecg?e=iZYhgz) and place it `Model/Anatomical_Segmentation/model.pth`.
+
+## Training
+
+---
+The proposed model can be trained with
+```
+Python trainer.py --data_path ../Data --subject_path ../Data/train_val_split.pickle --model_name KPNFeat --nf 16  --nk16 --loss_type l1_l1ana_1 --ana_weight 0.01
+```
+
+## Results
+
+---
+* Parameter Comparison
+    <div align="center">
+        <img src="resources/rs_00.png" width="550" />
+    </div>
+* Quantitative Results
+    <div align="center">
+        <img src="resources/rs_01.png" width="550" />
+    </div>
+* Qualitative Results
+    <div align="center">
+        <img src="resources/rs_02.png" width="550" />
+    </div>
+
+## Citation
+
+
+
--- train/trainer.py
+++ train/trainer.py
@@ -3,7 +3,6 @@ import argparse
 import pytorch_lightning as pl
 from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
 from datetime import datetime
-from Dataloader import get_Full_Denoising_Loader
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -11,8 +10,8 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path'       , type=str, default=r'../../Data')
-    parser.add_argument('--subject_path'    , type=str, default=r'../../Data/train_val_split.pickle')
+    parser.add_argument('--data_path'       , type=str, default=r'../Data')
+    parser.add_argument('--subject_path'    , type=str, default=r'../Data/train_val_split.pickle')
     parser.add_argument('--full_data'       , type=bool, default=False)
 
     parser.add_argument('--pin_memory'      , type=bool, default=True)
@@ -21,13 +20,13 @@ if __name__ == '__main__':
     parser.add_argument('--load_memory'     , type=bool, default=True)
     parser.add_argument('--window'          , type=int, default=[(-1024, 3072)])
 
-    parser.add_argument('--mocel_name'      , type=int, default='KPNFeat')
+    parser.add_argument('--model_name'      , type=str, default='KPNFeat')
     parser.add_argument('--patch_n'         , type=int)
     parser.add_argument('--patch_size'      , type=int)
     parser.add_argument('--nf'              , type=int, default=16)
     parser.add_argument('--nk'              , type=int, default=16)
 
-    parser.add_argument('--ana_model_path'  , type=str, default=r'../../Model/Anatomical_Segmentation/model.pth')
+    parser.add_argument('--ana_model_path'  , type=str, default=r'../Model/Anatomical_Segmentation/model.pth')
     parser.add_argument('--loss_type'       , type=str, default='l1_l1ana_1')
     parser.add_argument('--ana_weight'      , type=float, default=0.01)
     parser.add_argument('--lr'              , type=float, default=1e-4)
@@ -36,9 +35,9 @@ if __name__ == '__main__':
 
 
     parser.add_argument('--num_epochs'      , type=int, default=400)
-    parser.add_argument('--num_gpus'        , type=int, default=1)
+    # parser.add_argument('--num_gpus'        , type=int, default=1) # Deprecated
     # parser.add_argument('--swa'             , type=bool, default=True) # Deprecated
-    parser.add_argument('--precision'       , type=int, default=16)
+    parser.add_argument('--precision'       , type=str, default=16)
 
     args = parser.parse_args()
     model_name = '%s_nf%d_nk%d'%(args.model_name, args.nf, args.nk) if args.model_name == 'KPNFeat' else args.model_name
@@ -65,7 +64,7 @@ if __name__ == '__main__':
 
     lr_monitor = pl.callbacks.LearningRateMonitor(logging_interval='epoch')
     trainer = pl.Trainer(max_epochs = args.num_epochs,
-                         gpus = args.num_gpus,
+                         # gpus = args.num_gpus,
                          callbacks = [checkpointing, lr_monitor],
                          logger = tb_logger,
                          precision = args.precision,
@@ -76,7 +75,7 @@ if __name__ == '__main__':
                          )
 
 
-    from loader import get_Denoising_Loader
+    from loader import get_Full_Denoising_Loader
     train_loader, val_loader = get_Full_Denoising_Loader(args)
 
     # Model
